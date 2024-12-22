d5c440bf21e20f790504290dc72a0e63155cdd15
Ahmet Yasin Pekdemir <atlon34@outlook.com>
2024-12-22 23:19:17 +0300

    ASP.NET'e geçiş

---
+++ Controllers/HesapController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HesapController : Controller
+    {
+        private readonly ILogger<HesapController> _logger;
+
+        public HesapController(ILogger<HesapController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
