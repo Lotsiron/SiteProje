49027c5c23d6a77d9eb68ab333ea2857d4cebfb0
Ahmet Yasin Pekdemir <atlon34@outlook.com>
2024-12-25 23:30:17 +0300

    Randevu sistemine giriş

---
+++ Controllers/RandevuController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RandevuController : Controller
+    {
+        private readonly ILogger<RandevuController> _logger;
+
+        public RandevuController(ILogger<RandevuController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Salonlar(string salonTur)
+        {
+            var kuaforler = GetSalonTur(salonTur)
+
+            return View(salons);
+        }
+
+        // bazı yerlerde salon bazı yerlerde kuafor olarak geçiyor
+
+        private List<Salon> GetSalonTur(string salonTur)
+        {
+            using (var dbContext = new ApplicationDbContext())
+            {
+                // Tipine göre (erkek/kadın) kuaforleri getir
+
+                return dbContext.kuaforler
+                                .Where(s => s.Tur == salonTur)
+                                .ToList();
+            }
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
